22c6b9bde645b22ed3e83ed2fd3d41164f92e1a9
Pavan Kalyan <damalapati.pavan@gmail.com>
2022-11-11 16:53:47 -0500

    updated changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_03_002427) do
+ActiveRecord::Schema.define(version: 2022_10_30_210850) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -19,9 +19,10 @@ ActiveRecord::Schema.define(version: 2022_11_03_002427) do
   end
 
   create_table "stakes", force: :cascade do |t|
-    t.string "c_id"
-    t.string "u_id"
-    t.string "value"
+    t.string "company_name"
+    t.string "user_name"
+    t.string "stake"
+    t.integer "c_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
