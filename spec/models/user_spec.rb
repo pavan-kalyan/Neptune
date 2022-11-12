296dde04a0a38fcfbcc44809e6a66be6722ba601
Pavan Kalyan <damalapati.pavan@gmail.com>
2022-11-11 21:55:08 -0500

    100% coverage for user sign in and sign up

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "find by id" do
+
+    u = User.create!(email:"abc@gmail.com",
+                     password:"pass", name: "abc",
+                     role: "Employee", company_id: 1)
+    expect(User.get_user_name_by_id(u.id)==u.name).to be_truthy
+  end
 end
